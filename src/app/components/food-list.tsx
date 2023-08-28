94ab51d15c57bef15b9ab237449a0f77ca25e5ef
luix gabriel <luisgasbriel@gmail.com>
2023-08-28 16:35:32 -0300

    feat: add products

--- src/app/components/food-list.tsx
+++ src/app/components/food-list.tsx
@@ -1,42 +1,22 @@
+import burguerList from '@/data/burguers'
 import { Heart } from 'lucide-react'
 const FoodList = () => {
   return (
     <div className="mt-6 flex justify-center">
-      <div className=" w-[90%] p-3">
-        <h1 className="text-lg font-bold bg-blue-400 p-2">BURGUER</h1>
-        <div className=" flex items-center">
-          <div className="p-3">
-            <h4 className="font-semibold">Hamburgao</h4>
-            <span className="font-semibold">R$ 3,50</span>
-            <p className="text-xs">
-              Esse hamburgeu é muito muito muito gostoso e tbm é bem barto e
-              entrega no confrto da sua casa
-            </p>
+      <div className=" w-[95%] p-3">
+        <h1 className="text-lg font-bold bg-blue-400 p-2 rounded-sm">
+          BURGUER
+        </h1>
+        {burguerList.map((burger) => (
+          <div className=" flex items-center justify-between" key={burger.id}>
+            <div className="p-3 w-[80%]">
+              <h4 className="font-semibold">{burger.name}</h4>
+              <span className="font-semibold">{`R$ ${burger.price}`}</span>
+              <p className="text-xs">{burger.description}</p>
+            </div>
+            <Heart size={20} />
           </div>
-          <Heart />
-        </div>
-        <div className=" flex items-center">
-          <div className="p-3">
-            <h4 className="font-semibold">Hamburgao</h4>
-            <span className="font-semibold">R$ 3,50</span>
-            <p className="text-xs">
-              Esse hamburgeu é muito muito muito gostoso e tbm é bem barto e
-              entrega no confrto da sua casa
-            </p>
-          </div>
-          <Heart />
-        </div>
-        <div className=" flex items-center">
-          <div className="p-3">
-            <h4 className="font-semibold">Hamburgao</h4>
-            <span className="font-semibold">R$ 3,50</span>
-            <p className="text-xs">
-              Esse hamburgeu é muito muito muito gostoso e tbm é bem barto e
-              entrega no confrto da sua casa
-            </p>
-          </div>
-          <Heart />
-        </div>
+        ))}
       </div>
     </div>
   )
